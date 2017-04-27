653dd4c84cc17a815d737dcda2f0269622e0a732
dakotawashok <dakotawashok@gmail.com>
2017-04-27 15:35:09 -0500

    Merge branch 'Changes' of /Users/dakotawashok/NinjaDev/www/data-importer-actual with conflicts.

--- app/Console/Commands/ImportData.php
+++ app/Console/Commands/ImportData.php
@@ -20,7 +20,6 @@ use App\offer_media;
 use App\Offer_Translation;
 use App\OldOffer;
 use App\User;
-use App\user_custom;
 use App\user_to_app_to_role;
 use DateTime;
 use ErrorException;
@@ -78,29 +77,28 @@ class ImportData extends Command
 //        $this->parseImageMedia($mediaNames);
 //        $this->info('Done Importing Media Files!');
 
-//        $bar = $this->output->createProgressBar(count($fileNames));
-//
-//        foreach ($fileNames as $item) {
-//            if ($item != "." && $item != ".." && $item != '.DS_Store') {
-//                $offerContents = file_get_contents($folderUrl . '/' . $item);
-//                $offer = json_decode($offerContents);
-//
-//                if (is_array($offer) || is_object($offer)) {
-//                    $this->storeOldOffer($offer);
-//                } else {
-//                    $this->error('Error!');
-//                }
-//                //print_r($offer);
-//            }
-//            $bar->advance();
-//        }
-//
-//        $bar->finish();
-//
-//        $this->setDestinationAddresses();
-        $this->parseUserTable();
+        $bar = $this->output->createProgressBar(count($fileNames));
+
+        foreach ($fileNames as $item) {
+            if ($item != "." && $item != ".." && $item != '.DS_Store') {
+                $offerContents = file_get_contents($folderUrl . '/' . $item);
+                $offer = json_decode($offerContents);
+
+                if (is_array($offer) || is_object($offer)) {
+                    $this->storeOldOffer($offer);
+                } else {
+                    $this->error('Error!');
+                }
+            }
+            $bar->advance();
+        }
+
+        $bar->finish();
+
+        $this->setDestinationAddresses();
+//        $this->parseUserTable();
 //        $this->info('Multiples: ' . $this->multiples);
-//        $this->parseManualEntries();
+        $this->parseManualEntries();
     }
 
     /**
@@ -208,12 +206,6 @@ class ImportData extends Command
             $model->name = $location->correctedAddress;
             $model->lat = $location->point->lat;
             $model->lng = $location->point->lng;
-            $this->info('lat: ' . $location->point->lat);
-            $this->info('lng: ' . $location->point->lng);
-
-            $this->info('lat: ' . $model->lat);
-            $this->info('lng: ' . $model->lng);
-
             $model->offer_id = $oldOffer->coupon_id;
             $model->countrywide = false;
             $model->save();
@@ -649,8 +641,8 @@ class ImportData extends Command
             $modelUser = new User();
             $modelUser->name = $mostUser->user_login;
             $modelUser->username = $mostUser->user_login;
-            $modelUser->first_name = $mostUser->first_name;
-            $modelUser->last_name = $mostUser->last_name;
+            $modelUser->first_name = $mostUser->user_login;
+            $modelUser->last_name = '';
             $modelUser->last_login_date = null;
             $modelUser->email = $mostUser->user_email;
             $modelUser->password = null;
@@ -666,7 +658,6 @@ class ImportData extends Command
             $modelUser->last_modified_date = $today;
             $modelUser->created_by_id = 0;
             $modelUser->last_modified_by_id = 0;
-            $modelUser->id = $mostUser->id;
             $modelUser->save();
 
             $userToRole = new user_to_app_to_role();
@@ -675,27 +666,10 @@ class ImportData extends Command
             $userToRole->role_id = 7;
             $userToRole->save();
 
-            if ($mostUser->franchise != '' && !is_null($mostUser->franchise)) {
-                $this->parseFranchise($mostUser);
-            }
-
 
             $bar->advance();
         }
         $bar->finish();
-    }
-
-    public function parseFranchise($mostUser) {
-        $today = new DateTime('NOW');
-        $today = $today->format('Y-m-d H:i:s');
-
-        $franchise = new user_custom();
-        $franchise->user_id = $mostUser->id;
-        $franchise->name = 'franchise';
-        $franchise->value = $mostUser->franchise;
-        $franchise->created_date = $today;
-        $franchise->last_modified_date = $today;
-        $franchise->save();
     }
 
     ///////////////////////////////////////////////////////
